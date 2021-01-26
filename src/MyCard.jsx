60673d031d48f1eac6dba43c6a4b7808ab367b81
RRdD <rd@RDs-MacBook-Pro.local>
2021-01-26 08:54:20 +0200

    added hobby projects

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Mycards from "./MyCards";
 import { Grid } from "@material-ui/core";
 import dtList from "./ProjectData";
+import hobby from './HobbyData'
 import './Cards.css'
 
 const MyCard = () => {
@@ -20,6 +21,12 @@ const MyCard = () => {
     </div>
     <Grid container spacing={2}>
       {dtList.map(cardObj => getCard(cardObj))}
+    </Grid>
+    <div className="project_type">
+      <h3>Hobby Projects</h3>
+    </div>
+    <Grid container spacing={2}>
+      {hobby.map(cardObj => getCard(cardObj))}
     </Grid>
     </>
   );
