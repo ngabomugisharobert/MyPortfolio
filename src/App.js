7848205c892d9b7e9c661bce189fe1e8fd5ca141
RRdD <rd@RDs-MacBook-Pro.local>
2021-04-04 13:02:10 +0200

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
-import './App.css';
-import Navbar from './Navbar'
-import Skills from './Skills'
-import Tools from './Tools'
-import Glass from './Glass'
-import Timeline from './components/Timeline'
-import Projects from './Projects'
-import Info from './Info'
-import ReactGA from 'react-ga'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-function initAnalytics(){
-  ReactGA.initialize("G-92YJ1S8V8H")
-  ReactGA.pageview("/")
-}
+import React from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Stack from './components/Stack';
+import Work from './components/Work';
 
 function App() {
-  initAnalytics();
   return (
-
-
-
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Glass} />
-          <Route path='/skills' component={Skills} />
-          <Route path='/tools' component={Tools} />
-          <Route path='/experience' component={Timeline} />
-          <Route path='/projects' component={Projects} />
-        </Switch>
-        <Info />
-      </Router>
-    </div>
-
-
-
-
-
-   /* <div className="app">
-    <Head/>
-    <Glass/>
-      <Skills/>
-      <Tools/>
-    </div>*/
+    <>
+      <Navigation />
+      <Header />
+      <Work />
+      <Stack />
+      <Contact />
+    </>
   );
 }
 
